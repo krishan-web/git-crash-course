86a21faa0f96a08a9bab4dcddcf0aec49740b070
krishan-web <69138692+krishan-web@users.noreply.github.com>
2024-05-01 15:11:14 +0530

    Learned how to make a commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const h=26;
+console.log(h);
+
+// git init : to initialize the git in the current directory. 
+//example: 
+// git init
+// Initialized empty Git repository in C:/Users/Asus/Desktop/Git_repository/git-crash-course/.git/
