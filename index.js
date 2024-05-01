c7b405c8ff30f6dcfc22750ff273acfff0cc47f1
krishan-web <69138692+krishan-web@users.noreply.github.com>
2024-05-01 15:52:20 +0530

    New git commands added

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log(h);
 //example: 
 // git init
 // Initialized empty Git repository in C:/Users/Asus/Desktop/Git_repository/git-crash-course/.git/
+
+console.log("My name is krishan");
+// how to add changes :
+// (Add all the file)git add .  'or' (particular one file)git add <file name> 
+
+// To remove a file
+// git rm <file name>
+
+// git log : It will show all the commit i made in description.
+// git log --oneline: IT will show all the commit but in short way.
+// git show : it will all the changes we made.
+// git show commit_ID: it will the particular commit changes we made. 
+
+// Reverting Back:
+// git reset --hard commit_id
